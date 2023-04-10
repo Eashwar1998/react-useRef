3cd5ee24545a917d25043fbdda3c926726e747a6
Eashwar1998 <30700208+Eashwar1998@users.noreply.github.com>
2023-04-10 17:32:37 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useRef } from 'react';
+import './style.css';
 
 export default function App() {
+  const [start, setStart] = useState(null);
+  const [now, setNow] = useState(null);
+  const ref = useRef(null);
+  const handleStart = () => {
+    setStart(Date.now());
+    setNow(Date.now());
+    clearInterval(ref.current);
+    ref.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  };
+  const handleStop = () => {
+    clearInterval(ref.current);
+  };
+  let sp = 0;
+  if (start !== null && now !== null) {
+    sp = (now - start) / 1000;
+  }
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>
+        Elasped Time : <span>{sp.toFixed(3)}</span>
+      </p>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 }
